77761ef424c6b5114e43b7e177206aa6a75ba264
Luis Palumbo <93258543+luispalumbo@users.noreply.github.com>
2026-06-04 11:29:42 +0800

    Update script.js
    changed the hopduration variable to 3000

--- js/script.js
+++ js/script.js
@@ -264,7 +264,7 @@ function sendPing(srcId, dstId) {
   circle.setAttribute('class', 'packet');
   svgLayer.appendChild(circle);
   
-  const hopDuration = 2000;
+  const hopDuration = 3000;
   let currentHop = 0;
   let startTime = performance.now();
 
